e70b0b63f4ddddebf7ddcd293e801f6f64116f0c
Chi Barnett <cbarnett@winkapp.com>
2017-06-15 22:39:55 -0700

    Works for now (with canned soup)

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-OMG WTF PZA
+# PIZZA.PY
+##OMG WTF PZA
+
+Cheapie review aggregator. Uses the Yelp API to grab your pizza place, then scrapes reviews and averages the N most recent.
+
+### Without Docker
+* Install the required libraries
+    ```pip install -r requirements.txt```
+* Export the app name and run Flask
+    ```export FLASK_APP=pizza.py && flask run```
+* Use your web browser to go find a New York slice
+    ```http://localhost:5000```
+
--- pizza.py
+++ pizza.py
@@ -7,11 +7,12 @@ import os
 import json
 from datetime import date
 from urllib import urlencode
-from collections import OrderedDict
+from flask import Flask, render_template, request
 
 BASE_URL='https://www.yelp.com/'
 TITLE_TO_RATING = r'(\d\.\d) star rating'
 YELP_DATE_MATCHER = r'(\d{1,2})/(\d{1,2})/(\d{4})'
+DEFAULT_REVIEW_COUNT = 5
 
 def has_class(tag, class_str):
   return 'class' in tag.attrs and class_str in tag.attrs.get('class')
@@ -50,11 +51,13 @@ def get_raw_ratings(reviews_divs):
   filter(is_star_div, map(lambda x: x.find_all('div'), reviews_divs))
 
 def soup_url(url):
-  response = request.get(url)
-  return BeautifulSoup(response.content, 'html.parser')
+    # pretend we're chrome to be less obvious robots
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    return BeautifulSoup(response.content, 'html.parser')
 
-# ratings must be scraped after we have the biz's url
-def get_ratings(soup):
+# reviews must be scraped after we have the biz's url
+def get_reviews(soup):
   review_divs = filter(lambda x: has_class(x, 'review'), soup.find_all('div'))
   # pop off the first one of these, it's just us and our non-existent review
   review_divs.pop(0)
@@ -70,10 +73,12 @@ def get_ratings(soup):
 def avg(values):
   return sum(values)/len(values)
 
-def get_n_ratings_with_avg(soup, num_ratings):
-  ratings = get_ratings(soup)[0:num_ratings]
-  avg_rating = avg(filter(lambda x: x is not None, map(lambda x: x['rating'], ratings)))
-  return (avg_rating, ratings)
+def get_n_reviews_with_avg(soup, num_reviews):
+  reviews = get_reviews(soup)[0:num_reviews]
+  avg_rating = avg(filter(lambda x: x is not None, map(lambda x: x['rating'], reviews)))
+  return {"average_rating": avg_rating,
+          "reviews": reviews}
+
 
 def refresh_token(secret_id, secret):
   url = 'https://api.yelp.com/oauth2/token'
@@ -87,34 +92,81 @@ def refresh_token(secret_id, secret):
 # returns raw request. this is what we refresh/retry if the token is expired.
 def do_pizza_search(name):
   url = 'https://api.yelp.com/v3/businesses/search'
-  params = {'keyword': name,
+  params = {'term': name,
     "location": 'New York, NY, US',
     'categories': 'pizza',
     'limit': 1
     }
+  # authorize
   headers = {'Authorization': "Bearer %s" % os.environ.get('PIZZA_ACCESS_TOKEN')}
   return requests.get(url, params=params, headers=headers)
 
 def find_pizza(name):
+  # cache this in the env.
+  if os.environ.get('PIZZA_ACCESS_TOKEN') is None:
+    refresh_token('7gck0xCgh1VbY6n5yLd_YA', 'yydTTgHU3blJFEvZOSe5UzLn9diqBhMvwiEj8Y50aSwXObZIxq9xldqsRTBklmMy')
   pizza_response = do_pizza_search(name)
   if pizza_response.status_code / 100 == 4:
     # auth probs, refresh and try again
-    refresh_token
+    refresh_token('7gck0xCgh1VbY6n5yLd_YA', 'yydTTgHU3blJFEvZOSe5UzLn9diqBhMvwiEj8Y50aSwXObZIxq9xldqsRTBklmMy')
     pizza_response = do_pizza_search(name)
   if pizza_response.status_code != 200:
     raise IOError("Can't get pizza! %d: %s" \
       % (pizza_response.status_code, pizza_response.content))
   print pizza_response.json()
   results = pizza_response.json().get('businesses', [])
-  url = results[0]['url'] if len(results) > 0 else None
-  return url
+  return results[0] if len(results) > 0 else None
 
-if __name__=='__main__':
+app = Flask(__name__)
+
+@app.template_filter('pretty_date')
+def pretty_date(d):
+    return d.isoformat()
+
+@app.route('/')
+def pizza_finder():
+    app.config["CACHE_TYPE"] = "null"
+    return render_template('search.html')
+
+@app.route('/find', methods=['POST', 'GET'])
+def pizza_display():
+    app.config["CACHE_TYPE"] = "null"
+    pizza_name = None
+    review_count = None
+    if request.method == 'POST':
+      pizza_name = request.form.get('name')
+      review_count = request.form.get('count', DEFAULT_REVIEW_COUNT)
+    elif request.method == 'GET':
+      pizza_name = request.args.get('name')
+      review_count = request.args.get('count', DEFAULT_REVIEW_COUNT)
+    # coercion for the count since we will get a string
+    try:
+      review_count = int(review_count)
+    except ValueError:
+      review_count = DEFAULT_REVIEW_COUNT
+    if pizza_name is None:
+      print "Request:" + request.__repr__()
+      print "Form:" + str(request.form)
+      return render_template('search.html')
+    else:
+      result = find_pizza(pizza_name)
+      pizza_url = result['url'].split('?')[0]
+      pizza_name = result['name']
+      #pizza_soup = soup_url(pizza_url)
+      #print pizza_soup
+      canned_soup = None
+      with open('fbc.htm', 'r') as myfile:
+        canned_soup = BeautifulSoup(myfile.read(), 'html.parser')
+      pizza_reviews = get_n_reviews_with_avg(canned_soup, review_count)
+      return render_template('search.html', results=pizza_reviews, pizza_name=pizza_name)
+
+#if __name__=='__main__':
   # cache this in the env.
-  if os.environ.get('PIZZA_ACCESS_TOKEN') is None:
-    refresh_token('7gck0xCgh1VbY6n5yLd_YA', 'yydTTgHU3blJFEvZOSe5UzLn9diqBhMvwiEj8Y50aSwXObZIxq9xldqsRTBklmMy')
-  url = find_pizza('italy')
-  print url
-  with open('fbc.htm', 'r') as myfile:
-    canned_soup = BeautifulSoup(myfile.read(), 'html.parser')
-    print get_n_ratings_with_avg(canned_soup, 5)
+#  if os.environ.get('PIZZA_ACCESS_TOKEN') is None:
+#    refresh_token('7gck0xCgh1VbY6n5yLd_YA', 'yydTTgHU3blJFEvZOSe5UzLn9diqBhMvwiEj8Y50aSwXObZIxq9xldqsRTBklmMy')
+#  url = find_pizza('italy')
+#  print url
+#  with open('fbc.htm', 'r') as myfile:
+#    canned_soup = BeautifulSoup(myfile.read(), 'html.parser')
+#    print get_n_ratings_with_avg(canned_soup, 5)
+#
